30c4ec392531f5a8065ac3da126a88186d0cba22
Tara <tarark@mit.edu>
2024-12-16 10:26:54 -0500

    fixed library.js

--- webppl-model/src/library.js
+++ webppl-model/src/library.js
@@ -1,10 +1,8 @@
-// Javascript code goes here
 const fs = require('fs');
 
 var exampleJavascriptFn = function(x) {
-  return x + 1;
+    return x + 1;
 }
-
 
 var word_freq = JSON.parse(fs.readFileSync("word_freq.json", "utf8"));
 var dictionary = Object.keys(word_freq);
@@ -12,97 +10,114 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 // Utility: sum function
 function sum(arr) {
-  return arr.reduce(function(acc, x){ return acc + x; }, 0);
+    return arr.reduce(function(acc, x){ return acc + x; }, 0);
 }
 
 function match(pattern, word) {
-  if (word.length !== pattern.partial.length) {
-    return false;
-  }
-  for (var i = 0; i < word.length; i++) {
-    if (pattern.partial[i] === "_") {
-      if (pattern.not_in.indexOf(word[i]) > -1) {
+    if (word.length !== pattern.partial.length) {
         return false;
-      }
-      if (pattern.in.indexOf(word[i]) > -1) {
-        return false;
-      }
-    } else {
-      if (pattern.partial[i] !== word[i]) {
-        return false;
-      }
     }
-  }
-  return true;
+    for (var i = 0; i < word.length; i++) {
+        if (pattern.partial[i] === "_") {
+            if (pattern.not_in.indexOf(word[i]) > -1) {
+                return false;
+            }
+            if (pattern.in.indexOf(word[i]) > -1) {
+                return false;
+            }
+        } else {
+            if (pattern.partial[i] !== word[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 function get_possible_word(pattern) {
-  // use native .filter
-  return dictionary.filter(function(w) {
-    return match(pattern, w);
-  });
+    return dictionary.filter(function(w) {
+        return match(pattern, w);
+    });
 }
 
 function get_likelyhoods(pattern, words, use_word_frequency) {
-  if (use_word_frequency === undefined) {
-    use_word_frequency = false;
-  }
-  var total = use_word_frequency ? 0 : words.length;
-  var letter_counts = {};
-  for (var i = 0; i < alphabet.length; i++) {
-    letter_counts[alphabet[i]] = 0;
-  }
-
-  for (var wi = 0; wi < words.length; wi++) {
-    var w = words[wi];
-    var weight = use_word_frequency ? (word_freq[w] || 0) : 1;
-    if (use_word_frequency) {
-      total += weight;
+    if (use_word_frequency === undefined) {
+        use_word_frequency = false;
     }
 
-    var wcopy = w;
-    for (var j = 0; j < w.length; j++) {
-      if (pattern.partial[j] !== "_") {
-        continue;
-      }
-      var letter = wcopy[j];
-      if (letter !== "$") {
-        letter_counts[letter] = (letter_counts[letter] || 0) + weight;
-        wcopy = wcopy.slice(0, j) + "$" + wcopy.slice(j+1);
-      }
+    // Initialize position-specific letter counts
+    var position_letter_counts = {};
+    for (var pos = 0; pos < pattern.partial.length; pos++) {
+        position_letter_counts[pos] = {};
+        for (var i = 0; i < alphabet.length; i++) {
+            position_letter_counts[pos][alphabet[i]] = 0;
+        }
     }
-  }
 
-  for (var k = 0; k < alphabet.length; k++) {
-    var l = alphabet[k];
-    letter_counts[l] = letter_counts[l] / total;
-  }
+    // Count letters by position
+    for (var wi = 0; wi < words.length; wi++) {
+        var w = words[wi];
+        var weight = use_word_frequency ? (word_freq[w] || 0) : 1;
 
-  return letter_counts;
+        for (var pos = 0; pos < w.length; pos++) {
+            if (pattern.partial[pos] === "_") {
+                var letter = w[pos];
+                position_letter_counts[pos][letter] += weight;
+            }
+        }
+    }
+
+    // Calculate position totals and normalize
+    var position_probabilities = {};
+    for (var pos = 0; pos < pattern.partial.length; pos++) {
+        if (pattern.partial[pos] === "_") {
+            var pos_total = sum(Object.values(position_letter_counts[pos]));
+            if (pos_total > 0) {  // Avoid division by zero
+                for (var letter of alphabet) {
+                    position_letter_counts[pos][letter] /= pos_total;
+                }
+            }
+        }
+    }
+
+    // Average probabilities across positions for each letter
+    var final_probabilities = {};
+    for (var letter of alphabet) {
+        var letter_probs = [];
+        for (var pos = 0; pos < pattern.partial.length; pos++) {
+            if (pattern.partial[pos] === "_") {
+                letter_probs.push(position_letter_counts[pos][letter]);
+            }
+        }
+        final_probabilities[letter] = letter_probs.length > 0 ? 
+            Math.max(...letter_probs) : 0;
+    }
+
+    return final_probabilities;
 }
 
-// Renamed from next_letter_likelihoods to letter_inference
 function letter_inference(pattern, use_word_frequency) {
-  if (use_word_frequency === undefined) {
-    use_word_frequency = false;
-  }
-  var possible = get_possible_word(pattern);
-  var likelihoods = get_likelyhoods(pattern, possible, use_word_frequency);
-
-  var guessed_letters = pattern.in.concat(pattern.not_in);
-  var filtered = {};
-  for (var i = 0; i < alphabet.length; i++) {
-    var letter = alphabet[i];
-    if (guessed_letters.indexOf(letter) === -1) {
-      filtered[letter] = likelihoods[letter];
+    if (use_word_frequency === undefined) {
+        use_word_frequency = false;
     }
-  }
 
-  return filtered;
+    var possible = get_possible_word(pattern);
+    var likelihoods = get_likelyhoods(pattern, possible, use_word_frequency);
+    
+    var guessed_letters = pattern.in.concat(pattern.not_in);
+    var filtered = {};
+    
+    for (var i = 0; i < alphabet.length; i++) {
+        var letter = alphabet[i];
+        if (guessed_letters.indexOf(letter) === -1) {
+            filtered[letter] = likelihoods[letter];
+        }
+    }
+    
+    return filtered;
 }
-
 
 module.exports = {
-  exampleJavascriptFn: exampleJavascriptFn,
-  letter_inference: letter_inference
+    exampleJavascriptFn: exampleJavascriptFn,
+    letter_inference: letter_inference
 }
